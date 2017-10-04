9fac88e5545e1cf844a9ed298daa028c4af1755a
Faraz <Faraz@Faraazs-MacBook-Pro.local>
2017-10-04 18:06:23 -0500

    viz part remaining now

--- Assignment_1/Assignment1.R
+++ Assignment_1/Assignment1.R
@@ -6,6 +6,7 @@ library("stringr")
 library("ggplot2")
 
 # reading the data
+
 acc2015 <- read_csv("2015/accident.csv")
 acc2015
 acc2014 <- read_sas("2014/accident.sas7bdat")
@@ -13,9 +14,9 @@ acc2014
 ls()
 
 # looking at which class functions() the objects follow
+
 class(acc2014)
 class(acc2015)
-
 # the class functions are of 1) class data.frame and 2) class tbl
 
 acc2014$TWAY_ID2
@@ -23,12 +24,10 @@ acc2014$TWAY_ID2
 # mutate strings in acc2014$RWAY_ID2 to NA
 
 acc2014 <- mutate(acc2014, TWAY_ID2 = na_if(TWAY_ID2, ""))
-
 table(is.na(acc2014$TWAY_ID2))
 
 # dim
 dim(acc2014)
-
 dim(acc2015)
 
 # identifying columns in one dataset but not in another
@@ -45,9 +44,35 @@ diff_columns_in_2015
 #  Columns in 2015 but not in 2014 dataset are "RUR_URB", "FUNC_SYS" and "RD_OWNER"
 
 # Combining two tibbles into one tibble
+
 acc <- bind_rows(acc2014, acc2015)
 
 # frequency table of the variable RUR_URB
-count(acc, RUR_URB) # 30,056 NA values exist because acc2014 dataset doesn't have this variable.
+
+count(acc, RUR_URB) 
+# 30,056 NA values exist because acc2014 dataset doesn't have this variable.
 # hence combining both datasets will create NA values for the acc2014 dataset
+
+# loading the "FIPS" dataset
+
+fips <- read_csv("Assignment 1/fips.csv")
+glimpse(fips)
+
+acc$STATE <- as.character(acc$STATE)
+acc$COUNTY <- as.character(acc$COUNTY)
+
+# Adding string pads to STATE and COUNTY variables of acc tibble
+
+acc$STATE <- str_pad(acc$STATE, 2, side="left", pad = 0)
+acc$COUNTY <- str_pad(acc$COUNTY, 3, side="left", pad = 0)
+
+# Rename variables names
+acc <- rename(acc, c("STATE" = "StateFIPSCode", "COUNTY" = "CountyFIPSCode"))
+acc
+# left join of two tibbles
+acc_merge <- left_join(acc, fips, by = c("StateFIPSCode" = "StateFIPSCode", "CountyFIPSCode" =  "CountyFIPSCode"))
+dim(acc_merge)
+dim(acc)
+#"acc" and "acc_merge" both have the same number of rows
+
 
