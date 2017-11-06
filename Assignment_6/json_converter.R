fdc2b21c623b0b36d142caff1ee0cc3bb0093c51
Faraz <Faraz@Faraazs-MacBook-Pro.local>
2017-11-05 19:19:10 -0600

    created a json object and started html.

---
+++ Assignment_6/json_converter.R
@@ -0,0 +1,24 @@
+library("dplyr")
+library("readr")
+library("ggplot2")
+library("lubridate")
+library("knitr")
+library("ggthemes")
+library("jsonlite")
+
+# import the data on relationship b/w dengue and age
+# Prevelance of Dengue Fever in Punjab 2011-12 (http://www.thejaps.org.pk/docs/Supplementary/v-25-sup-2/09.pdf)
+## Note that the data was converted from pdf into csv format
+dengue_age <- read_csv("Data/age_and_dengue_fever.csv") 
+dengue_age
+
+
+# convert it into a json
+
+library(jsonlite)
+dengue_age_json <- toJSON(dengue_age)
+dengue_age_json
+
+
+
+
