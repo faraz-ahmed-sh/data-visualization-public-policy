8a2243d70a69356e8094142284e0bd70591e326f
Faraz <Faraz@Faraazs-MacBook-Pro.local>
2017-11-09 17:54:56 -0600

    added title and subtitle to svg

--- Assignment_6/script.js
+++ Assignment_6/script.js
@@ -1,4 +1,6 @@
-var margin = 50; //define top, left, right and bottom margins in a dict
+
+
+var margin = 50;
 var width = 720;
 var height = 450;
 
@@ -8,6 +10,8 @@ var legendSpacing = 4;
 
 //load the data
 d3.json("dengue_age.json", function(error, data) {
+
+	if (error) throw error;
 
 	var data = data.filter(filterCriteria);
 	// filter the data as the last two rows are redundant for the purpose of this assignment.
@@ -36,7 +40,7 @@ d3.json("dengue_age.json", function(error, data) {
 	//in each neighborhood
 
 	data.sort(function(a, b) { return a["Total"] - b["Total"]; });
-	if (error) throw error;
+	
 	dataset = data;
 	makeBarChart(dataset);
 	
@@ -54,7 +58,7 @@ function makeBarChart(dataset) {
 	var svg = d3.select("#chart")
 		.append('svg') 
 		.attr('width', width + margin*2)
-		.attr('height', height + margin*1.5);
+		.attr('height', height + margin*3);
 
 	// set the domains of X and Y axes
 	var xScale = d3.scaleBand()
@@ -73,10 +77,10 @@ function makeBarChart(dataset) {
 	.range(["#12719e", "#1696d2", "#636363", "#46abdb", "#a2d4ec", "#73bfe2", "#a2d4ec"]);
 
 	var g = svg.append('g')
-	.attr('transform', 'translate(' + margin + ',' + (margin+10) + ')');
+	.attr('transform', 'translate(' + margin + ',' + (margin*2.5) + ')');
 	
 	var xAxis = svg.append('g')
-		.attr('transform', 'translate(' + margin + ',' + (margin+10) + ')')
+		.attr('transform', 'translate(' + margin + ',' + (margin*2.5) + ')')
 		.call(d3.axisTop(xScale))
 		.selectAll("text")
 		.attr("dx", "15")
@@ -84,22 +88,22 @@ function makeBarChart(dataset) {
 		.attr("transform", "rotate(-20)" );
 
 	var yAxis = svg.append('g')
-		.attr('transform', 'translate(' + margin + ',' + (margin+10) + ')')
+		.attr('transform', 'translate(' + margin + ',' + (margin*2.5) + ')')
 		.call(d3.axisLeft(yScale));
 
 	 //x and y axis labels              
      svg.append("text")    
-	    .attr("transform", "translate(" + (margin*8) + " ," + margin/4 + ")")
+	    .attr("transform", "translate(" + (margin*8) + " ," + (margin+30) + ")")
 	    .style("text-anchor", "middle")
 	    .attr("font-weight", "bold")
 	    .text("Neighborhood");
 
 	svg.append("text")
 	    .attr("transform", "rotate(-90)")
-	    .attr("x", -margin*8)
+	    .attr("x", -margin*9)
 	    .attr("y", margin/4)
 	    .attr("font-weight", "bold")
-	    .text("Number of people with dengue fever");
+	    .text("Number of people with dengue");
 
 	// add the data to the bar	chart
 	barchart = g.selectAll(".barchart")
@@ -119,7 +123,7 @@ function makeBarChart(dataset) {
 		.on("mouseout", function() { tooltip.style("display", "none"); })
 		.on("mousemove", function(d) {
 		    var xPosition = d3.mouse(this)[0] + (margin-15);
-		    var yPosition = d3.mouse(this)[1] + (margin-15);
+		    var yPosition = d3.mouse(this)[1] + (margin*2);
 		    tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
 		    tooltip.select("text").text(d[1]-d[0])});
 
@@ -150,13 +154,13 @@ function makeBarChart(dataset) {
       .attr('width', 15)                     
       .attr('height', 15)
       .attr("x", margin*3)
-      .attr("y", height)                
+      .attr("y", height+margin)                
       .style('fill', zScale)                          
       .style('stroke', zScale);                     
 
     legend.append('text')                        
       .attr('x', (margin)*3.38)          
-      .attr('y', height+11)          
+      .attr('y', height+margin+11)          
       .text(function(d) { return d; }); 
 
 	 // preparing the tooltip.
@@ -176,5 +180,20 @@ function makeBarChart(dataset) {
 		.style("text-anchor", "middle")
 		.attr("font-size", "12px")
 		.attr("font-weight", "bold");
+
+    svg.append("text")
+        .attr("x", (width/1.8))             
+        .attr("y", (margin / 2))
+        .attr("text-anchor", "middle")  
+        .style("font-size", "30px") 
+        //.font(sans-serif")
+        .text("High prevelance of dengue among young adults in Lahore");
+
+    svg.append("text")
+        .attr("x", (width/2.15))             
+        .attr("y", (margin))
+        .attr("text-anchor", "middle")  
+        .style("font-size", "20px") 
+        .text("Based on the survey conducted in 18 hospitals in Lahore in 2011-2012");
 
 };
