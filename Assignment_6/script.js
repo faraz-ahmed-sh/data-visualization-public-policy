ea5e78feebb0500a9e5c525b246376b85d64b902
Faraz <Faraz@Faraazs-MacBook-Pro.local>
2017-11-09 18:48:24 -0600

    final assignment ready to be submitted

--- Assignment_6/script.js
+++ Assignment_6/script.js
@@ -21,16 +21,16 @@ d3.json("dengue_age.json", function(error, data) {
 
 	data.forEach( function(d) {
 		// unary+ operator to read numerical data correctly.
-		d["01-10yrs."] = +d["01-10yrs."] || 0
-		d["11-20yrs."] = +d["11-20yrs."] || 0
-		d["21-30yrs."] = +d["21-30yrs."] || 0
-		d["31-40yrs."] = +d["31-40yrs."] || 0
-		d["41-50yrs."] = +d["41-50yrs."] || 0
-		d["51-60yrs."] = +d["51-60yrs."] || 0
-		d["61-70yrs."] = +d["61-70yrs."] || 0
-		d["71-80yrs."] = +d["71-80yrs."] || 0
-		d["81-90yrs."] = +d["81-90yrs."] || 0
-		d["91-100yrs."] = +d["91-100yrs."] || 0
+		d["01-10"] = +d["01-10yrs."] || 0
+		d["11-20"] = +d["11-20yrs."] || 0
+		d["21-30"] = +d["21-30yrs."] || 0
+		d["31-40"] = +d["31-40yrs."] || 0
+		d["41-50"] = +d["41-50yrs."] || 0
+		d["51-60"] = +d["51-60yrs."] || 0
+		d["61-70"] = +d["61-70yrs."] || 0
+		d["71-80"] = +d["71-80yrs."] || 0
+		d["81-90"] = +d["81-90yrs."] || 0
+		d["91-100"] = +d["91-100yrs."] || 0
 		d["Unknown"] = +d["Unknown"] || 0
 		d["Total"] = +d["Total"] || 0
 
@@ -50,7 +50,7 @@ function makeBarChart(dataset) {
 
 	//create a series of stacks/keys for the stacked bar chart for a limited set of categories
 	var series = d3.stack()
-    .keys(["01-10yrs.", "11-20yrs.", "21-30yrs.", "31-40yrs.", "41-50yrs.", "51-60yrs.", "61-70yrs."])
+    .keys(["01-10", "11-20", "21-30", "31-40", "41-50", "51-60", "61-70"])
     .offset(d3.stackOffsetDiverging)
     (dataset);
 
@@ -91,9 +91,9 @@ function makeBarChart(dataset) {
 		.attr('transform', 'translate(' + margin + ',' + (margin*2.5) + ')')
 		.call(d3.axisLeft(yScale));
 
-	 //x and y axis labels              
+	 //add x and y axis labels              
      svg.append("text")    
-	    .attr("transform", "translate(" + (margin*8) + " ," + (margin+30) + ")")
+	    .attr("transform", "translate(" + (width/2) + " ," + (margin+30) + ")")
 	    .style("text-anchor", "middle")
 	    .attr("font-weight", "bold")
 	    .text("Neighborhood");
@@ -105,19 +105,38 @@ function makeBarChart(dataset) {
 	    .attr("font-weight", "bold")
 	    .text("Number of people with dengue");
 
+	//add title and subtitle
+	svg.append("text")
+        .attr("x", (width/1.8))             
+        .attr("y", (margin / 2))
+        .attr("text-anchor", "middle")  
+        .style("font-size", "30px") 
+        //.font(sans-serif")
+        .text("High prevelance of dengue among young adults in Lahore");
+
+    svg.append("text")
+        .attr("x", (width/2.15))             
+        .attr("y", (margin))
+        .attr("text-anchor", "middle")  
+        .style("font-size", "20px") 
+        .text("Based on the survey conducted in 18 hospitals in Lahore in 2011-2012");
+
 	// add the data to the bar	chart
-	barchart = g.selectAll(".barchart")
+	var barchart = g.selectAll(".barchart")
 		.data(series) 
 		.enter().append("g")
 		.attr("fill", function(d) { return zScale(d.key); })
 		.selectAll("rect")
 		.data(function(d) { return d; })
 		.enter().append("rect")
+		
+		//stroke-width:5;fill-opacity:0.1;stroke-opacity:0.9")
 		.attr("class", "barchart") // now you're making sure that the class that you're choosing is barchartsource:
 		.attr("x", function(d) { return xScale(d.data["Towns name"]); })
 		.attr("width", "45")
 		.attr("y", function(d) { return yScale(d[0]); })
 		.attr("height", function(d) { return -(yScale(d[0]) - yScale(d[1])); })
+
 		//add the mouseover tooltip
 		.on("mouseover", function() { tooltip.style("display", null); })
 		.on("mouseout", function() { tooltip.style("display", "none"); })
@@ -138,7 +157,7 @@ function makeBarChart(dataset) {
 
 	//preparing the legend
 	var dataL = 5;
-	var offset = 65; // for spacing of the legend bars
+	var offset = 60; // for spacing of the legend bars
 
 	var legend = svg.selectAll('.legend')                     
       .data(zScale.domain())                                   
@@ -153,15 +172,24 @@ function makeBarChart(dataset) {
     legend.append('rect')                               
       .attr('width', 15)                     
       .attr('height', 15)
-      .attr("x", margin*3)
+      .attr("x", margin*2.4)
       .attr("y", height+margin)                
       .style('fill', zScale)                          
       .style('stroke', zScale);                     
 
     legend.append('text')                        
-      .attr('x', (margin)*3.38)          
+      .attr('x', (margin)*2.8)          
       .attr('y', height+margin+11)          
       .text(function(d) { return d; }); 
+
+  	//adding text above the legend labels
+    svg.append("text")
+        .attr("x", (width/2.0))             
+        .attr("y", (height+40))
+        .attr("text-anchor", "middle")  
+        .style("font-size", "15px") 
+        .attr("font-weight", "bold")
+        .text("Age groups"); 
 
 	 // preparing the tooltip.
 	var tooltip = svg.append("g")
@@ -180,20 +208,5 @@ function makeBarChart(dataset) {
 		.style("text-anchor", "middle")
 		.attr("font-size", "12px")
 		.attr("font-weight", "bold");
-
-    svg.append("text")
-        .attr("x", (width/1.8))             
-        .attr("y", (margin / 2))
-        .attr("text-anchor", "middle")  
-        .style("font-size", "30px") 
-        //.font(sans-serif")
-        .text("High prevelance of dengue among young adults in Lahore");
-
-    svg.append("text")
-        .attr("x", (width/2.15))             
-        .attr("y", (margin))
-        .attr("text-anchor", "middle")  
-        .style("font-size", "20px") 
-        .text("Based on the survey conducted in 18 hospitals in Lahore in 2011-2012");
 
 };
